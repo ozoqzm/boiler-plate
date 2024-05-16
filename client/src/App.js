ebd4dbc9550c3af67181230f24a6258ae3911395
SoJeong <105548648+ozoqzm@users.noreply.github.com>
2024-05-16 23:44:40 +0900

    Chore: proxy 설정

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
-import Auth from "./hoc/auth";
-import MovieDetail from "./components/views/MovieDetail/MovieDetail";
-import FavoritePage from "./components/FavoritePage/FavoritePage";
 import NavBar from "./components/views/NavBar/NavBar";
 
 function App() {
   return (
     <Router>
       <NavBar />
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/movie/:movieId" element={<MovieDetail />} />
-      <Route path="/favorite" element={<FavoritePage />} />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
     </Router>
   );
 }
