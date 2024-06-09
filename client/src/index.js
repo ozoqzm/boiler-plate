572b60215416ee66b40fdbd8f02fcd4ec65a832e
SoJeong <105548648+ozoqzm@users.noreply.github.com>
2024-06-09 22:13:05 +0900

    Chore: reducer 설정

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import promiseMiddleware from "redux-promise";
+import { thunk } from "redux-thunk";
+import Reducer from "./_reducers";
+
+const store = configureStore({
+  reducer: Reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(promiseMiddleware, thunk),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
